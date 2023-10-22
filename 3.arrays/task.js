479ec6a6e85fa2de655cdcef799198dcb11ededb
Alex Razygraev <rav200@mail.ru>
2023-10-22 09:17:59 +0300

    3 Задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
+'use strict'
+
 function compareArrays(arr1, arr2) {
-  
+  let result = arr1.length === arr2.length && arr1.every((number, item) => number === arr2[item]);
+
+  return result; // boolean
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+
+function advancedFilter(arr) {
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 ===0).map((item) => item * 10);
+
+    return resultArr; // array
 }
